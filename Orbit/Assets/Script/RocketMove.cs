f5db5c1065c62c4406266304dfbfe5d3ec3f7d97
fsmyth <finn.smyth97@gmail.com>
2021-04-21 20:26:27 +0100

    forward movement

---
+++ Orbit/Assets/Script/RocketMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMove : MonoBehaviour
+{
+
+    public Rigidbody sphere;
+    public float accForward = 5f, accBackward = 3f, speedMax = 30f, turnPower = 180;
+
+    private float isAccel, isTurning;
+
+    void Start()
+    {
+        sphere.transform.parent = null;
+    }
+
+    void Update()
+    {
+        isAccel = 0f;
+        if(Input.GetAxis("Vertical")>0) {
+            isAccel = Input.GetAxis("Vertical") * accForward * 1000f;
+        } else if (Input.GetAxis("Vertical")<0) {
+            isAccel = Input.GetAxis("Vertical") * accBackward * 1000f;
+        }
+
+        transform.position = sphere.transform.position;
+    }
+
+    void FixedUpdate(){
+        
+        if(Mathf.Abs(isAccel) > 0) 
+        {
+            sphere.AddForce(transform.up * isAccel);
+        }
+    }
+}

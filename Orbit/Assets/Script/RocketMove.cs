b34d863e7b1b7e19ffb207b5cad49152b07fa3c2
LiamConroy <liamconroy2012@gmail.com>
2021-04-26 20:55:21 +0100

    mini+third/first person

--- Orbit/Assets/Script/RocketMove.cs
+++ Orbit/Assets/Script/RocketMove.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class RocketMove : MonoBehaviour
 {
-
+    public GameObject ThirdPersonCam;
+    public bool ThirdCamOn = true;
+    public GameObject FirstPersonCam;
+    public bool FirstCamOn;
     public Rigidbody sphere;
     public float accForward = 5f, accBackward = 3f, speedMax = 30f, turnPower = 180, thrustPower = 10f, gravPower;
 
@@ -25,7 +28,7 @@ public class RocketMove : MonoBehaviour
             isAccel = Input.GetAxis("Vertical") * accBackward * 1000f;
         }
         if(Input.GetKey(KeyCode.Space)) {
-            sphere.AddForce(transform.forward * thrustPower * 10f);
+            sphere.AddForce(transform.forward * thrustPower * 100f);
             }
         
 
@@ -33,6 +36,21 @@ public class RocketMove : MonoBehaviour
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, isTurning * turnPower * Time.deltaTime, 0f));
 
         transform.position = sphere.transform.position;
+
+
+        if(Input.GetKey(KeyCode.P)){
+            if(ThirdCamOn){
+                FirstPerson();
+                ThirdCamOn = false;
+                Debug.Log("FP ON");
+            }
+
+            else{
+                ThirdPerson();
+                ThirdCamOn = true;
+                Debug.Log("TP ON");
+            }
+        }
     }
 
     void FixedUpdate(){
@@ -42,5 +60,41 @@ public class RocketMove : MonoBehaviour
         {
             sphere.AddForce(transform.up * isAccel);
         }
+    }
+
+    // void CamSwitch(){
+    //     if(!ThirdCamOn){
+    //            FirstPersonCam .SetActive(false);
+               
+
+    //            ThirdPersonCam.SetActive(true);
+
+    //            ThirdCamOn = true;
+    //            FirstCamOn= false;
+    //            Debug.Log("Third person");
+    //         }
+
+    //         else if(!FirstCamOn){
+    //            FirstPersonCam.SetActive(true);
+    //            FirstCamOn= true;
+
+    //            ThirdPersonCam.SetActive(false);
+    //            ThirdCamOn = false;mumumu
+
+    //            Debug.Log("First person");
+    //         }
+    // }
+
+    void FirstPerson() {   
+        ThirdPersonCam.SetActive(false);
+        FirstPersonCam.SetActive(true);   
+        
+        
+    }
+
+    void ThirdPerson() {
+        FirstPersonCam.SetActive(false); 
+        ThirdPersonCam.SetActive(true); 
+        
     }
 }
